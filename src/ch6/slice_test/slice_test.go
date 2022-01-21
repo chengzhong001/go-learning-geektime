d5dc0692b1f64c05d2b87e14854935408c03638c
azhong <zczwzw@163.com>
2022-01-21 20:27:28 +0800

    slice test

--- src/ch6/slice_test/slice_test.go
+++ src/ch6/slice_test/slice_test.go
@@ -14,5 +14,29 @@ func TestSliceInit(t *testing.T) {
 
 	s1 := []int{1, 2, 3, 4}
 	t.Log(len(s1), cap(s1))
-	
+
+	s2 := make([]int, 3, 5) //len: 3, cap: 5
+	t.Log(s2)
+	s2 = append(s2, 1)
+	t.Log(s2)
+}
+
+func TestSliceGrowing(t *testing.T) {
+	s := []int{}
+	for i := 0; i < 10; i++ {
+		s = append(s, i)
+		t.Log(len(s),cap(s))
+	}
+}
+
+
+func TestSliceShareMemory(t *testing.T){
+	year := [] string {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}
+	Q2 := year[3:6]
+	t.Log(Q2, len(Q2), cap(Q2))
+	summer := year[5:8]
+	t.Log(summer, len(summer), cap(summer))
+	summer[0] = "unknown"
+	t.Log(year)
+	t.Log(Q2)
 }
