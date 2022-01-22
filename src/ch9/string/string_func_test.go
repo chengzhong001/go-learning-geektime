891f7b98109a3fec04f823e71f02e535da99c40b
azhong <zczwzw@163.com>
2022-01-23 00:00:29 +0800

    strings func test

---
+++ src/ch9/string/string_func_test.go
@@ -0,0 +1,31 @@
+package string_test
+
+import (
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestString_func(t *testing.T){
+	s := "A,B,C"
+	parts := strings.Split(s, ",")
+	for _, part := range parts{
+		t.Log(part)
+	}
+
+	s = "中 华 人 民 共 和 国"
+	parts = strings.Split(s, " ")
+	for _, part := range parts{
+		t.Log(part)
+	}
+	t.Log(strings.Join(parts, "-"))
+}
+
+func TestConv(t *testing.T){
+	str := strconv.Itoa(10)
+	t.Log("str"+str)
+	if i, err := strconv.Atoi("10"); err==nil{
+		t.Log(10 + i)
+	}
+	
+}
--- src/ch9/string/string_test.go
+++ src/ch9/string/string_test.go
@@ -7,10 +7,23 @@ func TestString(t *testing.T){
 	t.Log(s)
 	s = "hello"
 	t.Log(len(s))
-	s = "中"
-	t.Log(s, len(s))
+
 	s = "\xE4\xb8\xa5"
 	t.Log(s, len(s))
 
+	s = "发展中国家"
+	c := []rune(s)
 
+	t.Logf("c utf-8 %x, and c's length is %d charater", c, len(c))
+
+	for i:=0; i<len(c); i++{
+		t.Log(c[i])
+	}
+}
+
+func TestStringToRune(t *testing.T){
+	s := "中华人民共和国"
+	for idx, c := range s{
+		t.Logf("%[1]c %[1]x %d", c, idx)
+	}
 }
