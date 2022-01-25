292e106860bbc5fd0e21a421846acbfd563914c3
azhong <zczwzw@163.com>
2022-01-25 23:25:23 +0800

    error test

---
+++ src/ch14/error/error_test.go
@@ -0,0 +1,24 @@
+package error_test
+
+import "testing"
+import "errors"
+
+func GetFibonacci(n int) ([]int, error) {
+	if n < 0 || n > 100 {
+		return nil, errors.New("p should be in [2,100]")
+	}
+	fibList := []int{1, 1}
+	for i := 2; i < n; i++ {
+		fibList = append(fibList, fibList[i-2]+fibList[i-1])
+	}
+	return fibList, nil
+}
+
+func TestGetFib(t *testing.T) {
+
+	if v, err := GetFibonacci(-10); err != nil {
+		t.Log(err)
+	} else {
+		t.Log(v)
+	}
+}
