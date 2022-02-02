62e3e10067013e373c6a704cdbac5ae84cfec035
azhong <zczwzw@163.com>
2022-02-02 22:37:02 +0800

    http router

--- src/ch37/http_router/http_router.go
+++ src/ch37/http_router/http_router.go
@@ -1,7 +1,24 @@
-package httprouter
+package main
 
-import "github.com/julienschmidt/httprouter"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-func Index()  {
-	
+	"github.com/julienschmidt/httprouter"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "welcome\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello %s!\n", ps.ByName("name"))
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
